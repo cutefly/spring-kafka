21adf045092b6c7be5aa5502313e26dddf5ba7bf
Chris Choi <prettyfly@korea.com>
2023-02-28 19:03:15 +0900

    first commit

---
+++ src/test/java/kr/co/kpcard/kafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kpcard.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
